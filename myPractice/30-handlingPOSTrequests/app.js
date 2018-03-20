35f90a9edf7a7e1e0f363de3cdcea57dff9d5a92
ACpython <acpython777@gmail.com>
2018-03-20 17:52:38 -0400

    Part way done 32

--- myPractice/30-handlingPOSTrequests/app.js
+++ myPractice/30-handlingPOSTrequests/app.js
@@ -1,5 +1,8 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 var app = express();
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.set('view engine', 'ejs');
 app.use('/assets', express.static('assets'));
@@ -12,6 +15,14 @@ app.get('/contact', function(req, res){
   console.log(req.query);
   res.render('contact', {qs: req.query});
   // res.render('contact');
+});
+
+// This handles form requests. We're sending POST data from transform
+// app.post is the server handling POST Data, and sending it to .EJS
+app.post('/contact', urlencodedParser, function(req, res){
+  console.log(req.body);
+  // res.render('contact', {qs: req.query});
+  res.render('contact-success', {data: req.body});
 });
 
 app.get('/profile/:name', function(req,res){
